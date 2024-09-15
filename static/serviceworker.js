20d67a93338902d7c2642d83fa260f4a489be4d3
dpshah23 <131801781+dpshah23@users.noreply.github.com>
2024-09-15 10:42:45 +0530

    update

--- static/serviceworker.js
+++ static/serviceworker.js
@@ -1,12 +1,29 @@
+const CACHE_NAME = 'my-cache-v1';
+
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open('my-cache').then((cache) => {
+        caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll([
                 '/',
                 '/static/css/style.css',
                 '/static/img/to-do-list.png',
-                '/static/img/to-do-list.png'
+                '/offline.html'  // Offline fallback page
             ]);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
         })
     );
 });
@@ -14,7 +31,9 @@ self.addEventListener('install', (event) => {
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
+            return response || fetch(event.request).catch(() => {
+                return caches.match('/offline.html');  // Return offline page if fetch fails
+            });
         })
     );
 });
