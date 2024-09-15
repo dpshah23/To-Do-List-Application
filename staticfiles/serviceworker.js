c6924af920ce0c2329b2feaa6940ba1ecb3ca9ea
dpshah23 <131801781+dpshah23@users.noreply.github.com>
2024-09-15 10:53:50 +0530

    update

---
+++ staticfiles/serviceworker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'my-cache-v1';
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll([
+                '/',
+                '/static/css/style.css',
+                '/static/img/to-do-list.png',
+                '/offline.html'  // Offline fallback page
+            ]);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request).catch(() => {
+                return caches.match('/offline.html');  // Return offline page if fetch fails
+            });
+        })
+    );
+});
